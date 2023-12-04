c651251a636a1ca33eb340b6b2b29507146d5c08
Min <minseok.km@gmail.com>
2023-12-04 15:09:16 -0500

    1

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,6 @@ from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .seeds import seed_commands
 from .config import Config
-from app.api.post_routes import post_routes
 
 app = Flask(__name__, static_folder='../react-app/build', static_url_path='/')
 
@@ -29,7 +28,6 @@ app.cli.add_command(seed_commands)
 app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
-app.register_blueprint(post_routes, url_prefix='/api/posts')
 db.init_app(app)
 Migrate(app, db)
 
