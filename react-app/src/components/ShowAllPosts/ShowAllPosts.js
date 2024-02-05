09ee83ddb570daa318a2cd498283dd0c079dc564
Min <minseok.km@gmail.com>
2024-02-05 16:39:54 -0500

    render fixes

--- react-app/src/components/ShowAllPosts/ShowAllPosts.js
+++ react-app/src/components/ShowAllPosts/ShowAllPosts.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchPosts } from '../../store/posts';
 import { Link } from 'react-router-dom'
+import './ShowAllPosts.css';
 
 const ShowAllPosts = () => {
     const dispatch = useDispatch();
@@ -12,17 +13,20 @@ const ShowAllPosts = () => {
     }, [dispatch]);
 
     return (
-        <div className="main-image-container">
-            {posts.map((post) => (
-                <Link key={post.id} to={`/post/${post.id}`}>
-                    <div className="post-image">
-                        <img
-                            src={post.image_url}
-                            alt="Post Image"
-                        />
-                    </div>
-                </Link>
-            ))}
+        <div className="header-text">Everyone's Otter photos
+            <div className="main-image-container">
+
+                {posts.map((post) => (
+                    <Link key={post.id} to={`/post/${post.id}`}>
+                        <div className="post-image">
+                            <img
+                                src={post.image_url}
+                                alt="Post Image"
+                            />
+                        </div>
+                    </Link>
+                ))}
+            </div>
         </div>
     );
 };
