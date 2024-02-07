7462e95e24e29f548044e34b9f42f55afc1d9853
Min <minseok.km@gmail.com>
2024-02-07 15:31:23 -0500

    today fixes

--- app/seeds/posts.py
+++ app/seeds/posts.py
@@ -7,35 +7,35 @@ def seed_posts():
 
     demo_post = Post(
         user_id=1,
-        post_name='Demo Post',
-        description='This is a demo post.',
+        post_name='Family outing',
+        description='Mother and cub otter floating in water',
         views=1234,
         upload_date='2023-12-04',
         url='https://example.com/demo-post',
         preview=True,
-        image_url='https://upload.wikimedia.org/wikipedia/commons/2/25/The_Krusty_Krab.png'  # Add the image URL
+        image_url='https://i.imgur.com/FRI4045.jpg'  # Add the image URL
     )
 
     marnie_post = Post(
         user_id=2,
-        post_name='Marnie Post',
-        description='This post belongs to Marnie.',
+        post_name='Lazy Sunday',
+        description='Sleek otter drying off in the sun',
         views=1234,
         upload_date='2023-12-05',
         url='https://example.com/demo-post',
         preview=True,
-        image_url='https://static.wikia.nocookie.net/spongebob/images/a/ac/SpongeBob%27sPlace.png'  # Add the image URL
+        image_url='https://i.imgur.com/qar6087.jpg'  # Add the image URL
     )
 
     bobbie_post = Post(
         user_id=3,
-        post_name='Bobbie Post',
-        description='This post belongs to Bobbie.',
+        post_name='Curiosity',
+        description='Cuter otter floating in the water',
         views=1234,
         upload_date='2023-12-05',
         url='https://example.com/demo-post',
         preview=True,
-        image_url='https://thespongeclub.com/wp-content/uploads/2022/08/Chum-Bucket-Guide.png'  # Add the image URL
+        image_url='https://i.imgur.com/t26Q9kW.jpg'  # Add the image URL
     )
 
     db.session.add(demo_post)
--- migrations/versions/597c5c2aeba0_.py
+++ migrations/versions/597c5c2aeba0_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 547af54087e1
+Revision ID: 597c5c2aeba0
 Revises: 
-Create Date: 2024-02-07 14:20:53.610717
+Create Date: 2024-02-07 15:27:59.907790
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '547af54087e1'
+revision = '597c5c2aeba0'
 down_revision = None
 branch_labels = None
 depends_on = None
