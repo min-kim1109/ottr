cf9630144c87d6c13a4ea36adb2f4942e0cc6421
Min <minseok.km@gmail.com>
2024-02-08 15:41:15 -0500

    fake one

--- migrations/versions/05729b27cf6d_.py
+++ migrations/versions/05729b27cf6d_.py
@@ -1,19 +1,16 @@
 """empty message
 
-Revision ID: 4029d1935bd6
-Revises:
-Create Date: 2024-02-08 15:27:01.407367
+Revision ID: 05729b27cf6d
+Revises: 
+Create Date: 2024-02-08 15:40:35.496664
 
 """
 from alembic import op
 import sqlalchemy as sa
 
-import os
-environment = os.getenv("FLASK_ENV")
-SCHEMA = os.environ.get("SCHEMA")
 
 # revision identifiers, used by Alembic.
-revision = '4029d1935bd6'
+revision = '05729b27cf6d'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -53,11 +50,6 @@ def upgrade():
     sa.ForeignKeyConstraint(['user_id'], ['users.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
-
-    if environment == "production":
-        op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
-        op.execute(f"ALTER TABLE posts SET SCHEMA {SCHEMA};")
-        op.execute(f"ALTER TABLE comments SET SCHEMA {SCHEMA};")
     # ### end Alembic commands ###
 
 
