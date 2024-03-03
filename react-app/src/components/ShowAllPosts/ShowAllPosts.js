e84ce143c7ebe04e782c2a7edcabeeab4b674feb
Min <minseok.km@gmail.com>
2024-03-03 02:45:50 -0500

    search feature implemented

--- react-app/src/components/ShowAllPosts/ShowAllPosts.js
+++ react-app/src/components/ShowAllPosts/ShowAllPosts.js
@@ -1,23 +1,33 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchPosts } from '../../store/posts';
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import './ShowAllPosts.css';
+
+const useQuery = () => {
+    return new URLSearchParams(useLocation().search);
+}
 
 const ShowAllPosts = () => {
     const dispatch = useDispatch();
     const posts = useSelector((state) => state.posts.posts);
+    const query = useQuery();
+    const searchQuery = query.get('search') || '';
 
     useEffect(() => {
         dispatch(fetchPosts());
     }, [dispatch]);
+
+    const filteredPosts = posts.filter(post =>
+        post.post_name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
 
     return (
         <div>
             <div className="top-text">Everyone's Otter photos</div>
             <div className="main-image-container">
 
-                {posts.map((post) => (
+                {filteredPosts.map((post) => (
                     <Link key={post.id} to={`/post/${post.id}`}>
                         <div className="post-image">
                             <img
