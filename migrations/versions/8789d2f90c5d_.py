983ae9c8e35b66fc68ed2b0106a0221121c92c51
Min <minseok.km@gmail.com>
2024-03-08 02:34:16 -0500

    seeder envs fixed

--- migrations/versions/8789d2f90c5d_.py
+++ migrations/versions/8789d2f90c5d_.py
@@ -1,12 +1,16 @@
 """empty message
 
 Revision ID: 8789d2f90c5d
-Revises: 
+Revises:
 Create Date: 2024-03-05 14:28:22.338199
 
 """
 from alembic import op
 import sqlalchemy as sa
+
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 
 # revision identifiers, used by Alembic.
@@ -50,7 +54,13 @@ def upgrade():
     sa.ForeignKeyConstraint(['user_id'], ['users.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
+
+    if environment == "production":
+        op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE posts SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE comments SET SCHEMA {SCHEMA};")
     # ### end Alembic commands ###
+
 
 
 def downgrade():
